4c7458d8fbcf47362211fdd60f87911e6e7f741a
Stefan Kebekus <stefan.kebekus@gmail.com>
2023-08-14 08:50:04 +0200

    Add command line options

--- main.cpp
+++ main.cpp
@@ -1,14 +1,34 @@
 
+#include <QCommandLineParser>
+
 #include "geoImage.h"
 
 int main(int argc, char *argv[])
 {
-    QString fileName = u"test.png"_qs;
+    QCoreApplication app(argc, argv);
 
+    QCommandLineParser parser;
+    parser.setApplicationDescription("Test for geoImages");
+    parser.addHelpOption();
+    parser.addPositionalArgument("image", "GeoTagged Image File");
+    parser.process(app);
+
+    const QStringList args = parser.positionalArguments();
+    if (args.size() != 1)
+    {
+        parser.showHelp(-1);
+    }
+
+    QString fileName = args[0];
     auto rectangle = GeoMaps::GeoImage::readCoordinates(fileName);
 
     qWarning() << u"Corner coordinates for image %1:"_qs.arg(fileName)
                << rectangle;
+
+    // Quick check if we can read the raster image
+    QImage img(fileName);
+    qWarning() << img;
+    img.save("t.png");
 
     return 0;
 }
